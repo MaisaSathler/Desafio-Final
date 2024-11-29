4aee031f4d1509d795ed3d0d3a8de9fd8de57a2c
Maisa Castro <maisacastro08@gmail.com>
2024-11-29 19:43:08 -0300

    att-doados end-queroDoar add-responsive

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,12 +1,25 @@
-   
+import S from "./doados.module.scss"
+import protagonista from "../../assets/protagonista.png"
    
    
    
    
    export default function Doados(){
     return(
-        <section>
-            <h2>Doados</h2>
+        
+        <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+        
+        <section className={S.boxBooks}>
+            <article>
+                <img src={protagonista} alt="" />
+                <h3>O protagonista</h3>
+                <p>Susanne Andrade</p>
+                <p>Ficção</p>
+            </article>
         </section>
+        </section>
+        
+        
     )
    }
